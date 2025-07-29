cf335f46f73a305a123d75a2de1cffee97ab62f1
kauanatomb <kauanatomb@gmail.com>
2025-07-29 10:25:58 +0200

    removed existent deadlock

--- philo/src/philo_utils1.c
+++ philo/src/philo_utils1.c
@@ -34,19 +34,24 @@ static int	should_stop_eating(t_philo *philo)
 	return (0);
 }
 
-static void	eat(t_philo *philo)
+static void	eat(t_philo *philo, pthread_mutex_t *first, pthread_mutex_t *second)
 {
 	if (should_stop_eating(philo))
 		return ;
-	pthread_mutex_lock(philo->r_fork);
+	if (first > second)
+	{
+		first = philo->l_fork;
+		second = philo->r_fork;
+	}
+	pthread_mutex_lock(first);
 	print_action(philo, "has taken a fork");
 	if (philo->data->n_philo == 1)
 	{
 		safe_usleep(philo->data->time_to_die);
-		pthread_mutex_unlock(philo->r_fork);
+		pthread_mutex_unlock(first);
 		return ;
 	}
-	pthread_mutex_lock(philo->l_fork);
+	pthread_mutex_lock(second);
 	print_action(philo, "has taken a fork");
 	pthread_mutex_lock(&philo->meal_mutex);
 	philo->n_meals++;
@@ -54,20 +59,24 @@ static void	eat(t_philo *philo)
 	pthread_mutex_unlock(&philo->meal_mutex);
 	print_action(philo, "is eating");
 	safe_usleep(philo->data->time_to_eat);
-	pthread_mutex_unlock(philo->r_fork);
-	pthread_mutex_unlock(philo->l_fork);
+	pthread_mutex_unlock(first);
+	pthread_mutex_unlock(second);
 }
 
 void	*philosopher_routine(void *arg)
 {
-	t_philo	*philo;
+	t_philo			*philo;
+	pthread_mutex_t	*first;
+	pthread_mutex_t	*second;
 
 	philo = (t_philo *)arg;
 	if (philo->id % 2 == 0)
 		safe_usleep(10);
+	first = philo->r_fork;
+	second = philo->l_fork;
 	while (!is_simulation_stopped(philo->data))
 	{
-		eat(philo);
+		eat(philo, first, second);
 		print_action(philo, "is sleeping");
 		safe_usleep(philo->data->time_to_sleep);
 		print_action(philo, "is thinking");
